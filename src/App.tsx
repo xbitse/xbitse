d5a7e513c9a3776415f04af5df0c1c5586b90d0f
XBit Development Team <info@xbit.se>
2026-03-23 10:23:15 +0100

    fix: correct HashRouter closing tag

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ThemeProvider>
